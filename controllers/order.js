83264c472a1bdf22258665aecdcf42ed11493c37
huntdavid175 <huntdavid175@gmail.com>
2023-03-15 12:17:32 +0000

    creating an order done

--- controllers/order.js
+++ controllers/order.js
@@ -3,18 +3,31 @@ const Menu = require("../models/menu");
 const createOrder = async (req, res) => {
   const ids = req.body;
   const objectIds = [
-    "64103e02d6b39782c6f78d07",
-    "64103e02d6b39782c6f78d01",
-    "64103e02d6b39782c6f78d26",
-    "64103e02d6b39782c6f78d27",
+    "6411a6f0affc08795b724cef",
+    "6411a6f0affc08795b724cf0",
+    "6411a6f0affc08795b724d06",
+    "6411a6f0affc08795b724d12",
+    "6411a6f0affc08795b724d13",
+    "6411a6f0affc08795b724d22",
   ];
   try {
     const response = await Menu.find(
-      { foodList: { $elemMatch: { _id: { $in: objectIds } } } },
-      { "foodList.$": 1 }
+      { foodList: { $elemMatch: { _id: { $in: objectIds } } } }
+      // { "foodList.$": 1 }
     );
     if (response) {
-      res.json({ data: response });
+      const food = response.map((obj) => obj.foodList).flat();
+
+      const exactFood = objectIds.map((id) => {
+        let item;
+        food.forEach((food) => {
+          if (id === food._id.toString()) {
+            item = food;
+          }
+        });
+        return item;
+      });
+      res.json({ data: exactFood });
     }
   } catch (error) {
     console.log(error);
